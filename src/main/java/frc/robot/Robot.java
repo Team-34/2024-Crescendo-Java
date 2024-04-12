ab6ea80619f13f5ecb566bcbb83f6792cd608ec3
Brad Collins <bsc@housecollins.com>
2024-04-11 22:38:12 -0500

    Added back everything plus Robot.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -15,89 +16,278 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+    private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
+    private RobotContainer m_robotContainer;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
-  }
+    private boolean bypass = false;
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
 
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
+    /**
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
+      // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+      // autonomous chooser on the dashboard.
+      this.m_robotContainer = new RobotContainer();
 
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+      this.m_robotContainer.shooter.init();
+      // this.m_robotContainer.climber.init();
     }
-  }
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
+    /**
+     * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+     * that you want ran during disabled, autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+        // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+        // commands, running already-scheduled commands, removing finished or interrupted commands,
+        // and running subsystem periodic() methods.  This must be called from the robot's periodic
+        // block in order for anything in the Command-based framework to work.
+        CommandScheduler.getInstance().run();
 
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+        //static std::shared_ptr<t34::SwerveDrive> drive = m_this.m_robotContainer.SwerveDrive;
+        final Gyro gyro = Gyro.get();
+        
+        SmartDashboard.putNumber("_Yaw", gyro.getAngle());
+    
+        this.m_robotContainer.swerve_drive.putTelemetry();
+        this.m_robotContainer.shooter.putTelemetry();
+    
+        //Periodics
+        this.m_robotContainer.shooter.periodic();
+        // this.m_robotContainer.climber.periodic();
+        this.m_robotContainer.limelight_util.periodic();
+        //_________________________
+    
+        // Swerve Auto Drive Outputs
+        SmartDashboard.putNumber("Auto drive x: ", this.m_robotContainer.limelight_util.m_swerve_drive_speeds.x);
+        SmartDashboard.putNumber("Auto drive y: ", this.m_robotContainer.limelight_util.m_swerve_drive_speeds.y);
+        SmartDashboard.putNumber("Auto drive r: ", this.m_robotContainer.limelight_util.m_swerve_drive_speeds.r);
+        //_________________________
+    
+    
+        // Misc.
+        SmartDashboard.putNumber("Target ID: ", this.m_robotContainer.limelight_util.getTargetID());
+        SmartDashboard.putNumber("Distance from limelight target (meters): ", this.m_robotContainer.limelight_util.m_math_handler.getDistanceFromTarget());
+        SmartDashboard.putBoolean("Note Sensor detection", this.m_robotContainer.shooter.intakeHasNote());
+        SmartDashboard.putBoolean("Arm Sensor detection", this.m_robotContainer.shooter.isArmAtZero());
+    
+        SmartDashboard.putNumber("Raw Arm Encoder Val: ", this.m_robotContainer.shooter.getTopArmEncoderVal() / Constants.ARM_ENC_CONVERSION_FACTOR);
+    
+        //_________________________
+        /*
+        //checks if the approx. range is nearing 5 meters (the range the LL with pick up an AT before the resolution becomes too low)
+        if (log2( (2.5 / LimelightHelpers::getTA()) - 0.3) >= 5.0) {
+            SmartDashboard.putNumber("Over maximum detection range, current distance is: ", this.m_robotContainer.limelight_util.m_math_handler.GetDistanceFromTarget());
+        }
+        //_________________________*/
+        
+        
+        SmartDashboard.putData("Auto chooser: ", this.m_robotContainer.path_chooser);
+    
+        this.m_robotContainer.limelight_util.m_math_handler.putTelemetry();      
     }
-  }
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
+    /** This function is called once each time the robot enters Disabled mode. */
+    @Override
+    public void disabledInit() {}
 
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
+    @Override
+    public void disabledPeriodic() {}
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+    /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+    @Override
+    public void autonomousInit() {
+        Gyro.get().zeroYaw();
 
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
+        m_autonomousCommand = this.m_robotContainer.getAutonomousCommand();
 
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
+        // schedule the autonomous command (example)
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.schedule();
+        }
+    }
+
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {}
+
+    @Override
+    public void teleopInit() {
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.cancel();
+        }
+
+        this.m_robotContainer.shooter.setTolerance(0.03);
+        this.m_robotContainer.shooter.setkP(0.5);
+
+        CommandScheduler.getInstance().setDefaultCommand(
+            this.m_robotContainer.swerve_drive,
+            this.m_robotContainer.DefaultCommand);
+    }
+
+    /** This function is called periodically during operator control. */
+    @Override
+    public void teleopPeriodic() {
+        final Gyro gyro = Gyro.get();
+    
+        this.m_robotContainer.traj_math.inputMotorOutputPercent(this.m_robotContainer.shooter.getMaxSpeedPercent());
+    
+        // PROCESS CONTROLLER BUTTONS
+        // Buttons are implemented this way out of simplicity.
+        // Consider using button trigger events with commands instead.
+    
+        // Assign Back Button to Faris Mode.
+        if (this.m_robotContainer.ctrl.getBackButtonReleased())
+        {
+            this.m_robotContainer.swerve_drive.toggleFarisMode();
+        }
+    
+        // Assign Start Button to Zeroing Yaw.
+        // Note: This is for emergency use only!
+        // The robot should be oriented with the front pointed 
+        // at the opposite end of the field and sides as 
+        // parallel as possible to the fields sides when this
+        // button is pressed/released.
+        if (this.m_robotContainer.ctrl.getStartButtonReleased()) {
+            gyro.zeroYaw();
+        }
+    
+        // toggle PID vs basic motor output arm movement with the A button
+        if (this.m_robotContainer.ctrl.getYButtonReleased()) { 
+            this.m_robotContainer.arm_angle_setpoint = ((this.m_robotContainer.shooter.getTopArmEncoderVal() + this.m_robotContainer.shooter.getBottomArmEncoderVal()) * 0.5) / Constants.ARM_DEG_SCALAR;
+            this.m_robotContainer.shooter.togglePIDArmMovement();
+        }
+    
+        //if (this.m_robotContainer.ctrl.getXButton() == false && (this.m_robotContainer.ctrl.getRightTriggerAxis() < this.m_robotContainer.ctrl.getRightTriggerDB()))
+        //{
+        //    this.m_robotContainer.shooter.runIntakeMotorPercent(0.0);
+        //}
+    
+        //Run the shooter with the triggers
+          //Right is forward, left is back
+        if (this.m_robotContainer.ctrl.getLeftTriggerAxis() > 0.2)
+        {
+            bypass = false;
+            this.m_robotContainer.shooter.runShooterPercent(-(this.m_robotContainer.ctrl.getLeftTriggerAxis()));
+        }
+        else if (this.m_robotContainer.ctrl.getRightTriggerAxis() > 0.2)
+        {
+            bypass = true;
+            this.m_robotContainer.shooter.runShooterPercent(this.m_robotContainer.ctrl.getRightTriggerAxis());
+        }
+        else
+        {
+            bypass = false;
+            this.m_robotContainer.shooter.runShooterPercent(0.0);
+        }
+    
+        //Set the robot's target mode with the D-Pad
+        switch (this.m_robotContainer.ctrl.getPOV())
+        {
+            case (Constants.POV_UP): //  rest
+                this.m_robotContainer.shooter.configForRest();
+                
+                break;
+            case (Constants.POV_RIGHT): // amp
+                this.m_robotContainer.limelight_util.targetAmp();
+                this.m_robotContainer.shooter.configForAmp();
+                break;
+            case (Constants.POV_DOWN): // note collection
+                
+                this.m_robotContainer.shooter.configForNoteCollection();
+                break;
+            case (Constants.POV_LEFT): // max speed
+                this.m_robotContainer.limelight_util.targetSpeaker();
+                this.m_robotContainer.shooter.configForSpeaker(this.m_robotContainer.traj_math.getFiringAngleDeg());
+                break;
+        }
+    
+        //Move the arm with the bumpers
+          //Right bumper increases angle, left bumper decreases angle
+    
+        if (this.m_robotContainer.ctrl.getLeftBumper() && this.m_robotContainer.shooter.usingPIDArmMovement() && this.m_robotContainer.shooter.isArmAtZero() == false)
+        {
+            this.m_robotContainer.shooter.moveDown();
+        }
+        else if (this.m_robotContainer.ctrl.getRightBumper() && this.m_robotContainer.shooter.usingPIDArmMovement() == false)
+        {
+            this.m_robotContainer.shooter.runTopArmMotorPercent(0.25);
+            this.m_robotContainer.shooter.runBottomArmMotorPercent(0.25);
+        }
+        else if (this.m_robotContainer.ctrl.getRightBumper() && this.m_robotContainer.shooter.usingPIDArmMovement())
+        {
+            this.m_robotContainer.shooter.moveUp();
+        }
+        else if (this.m_robotContainer.ctrl.getLeftBumper() && this.m_robotContainer.shooter.usingPIDArmMovement() == false && this.m_robotContainer.shooter.isArmAtZero() == false)
+        {
+            this.m_robotContainer.shooter.runTopArmMotorPercent(-0.3);
+            this.m_robotContainer.shooter.runBottomArmMotorPercent(-0.3);
+        }
+        else if (this.m_robotContainer.shooter.usingPIDArmMovement() == false)
+        {
+            this.m_robotContainer.shooter.runTopArmMotorPercent(0.0);
+            this.m_robotContainer.shooter.runBottomArmMotorPercent(0.0);
+        }
+    
+        //Run intake backward with the X button, forward with A button
+        if (this.m_robotContainer.ctrl.getXButton())
+        {
+            this.m_robotContainer.shooter.runIntakeMotorPercent(-0.5);
+        }
+        else if (this.m_robotContainer.ctrl.getAButton())
+        {
+            this.m_robotContainer.shooter.runIntakeMotorPercent(0.5, bypass);
+        }
+        else
+        {
+            if (!bypass)
+                this.m_robotContainer.shooter.runIntakeMotorPercent(0.0, bypass);
+        }
+    
+        //if (this.m_robotContainer.ctrl.getYButton()) // run swerve automatically using the limelight with the Y button
+        //{
+        //  frc2::Command([this]
+        //  {
+        //      this.m_robotContainer.swerve_drive->Drive(
+        //          frc::Translation2d(
+        //              units::meter_t(this.m_robotContainer.limelight_util.m_swerve_drive_speeds.x),
+        //              units::meter_t(this.m_robotContainer.limelight_util.m_swerve_drive_speeds.y)
+        //              ), this.m_robotContainer.limelight_util.m_swerve_drive_speeds.r
+        //      );
+    //
+        //  }).Schedule();
+        //}
+    
+        this.m_robotContainer.shooter.periodic();
+    }
+
+    @Override
+    public void testInit() {
+        // Cancels all running commands at the start of test mode.
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {}
+
+    /** This function is called once when the robot is first started up. */
+    @Override
+    public void simulationInit() {}
+
+    /** This function is called periodically whilst in simulation. */
+    @Override
+    public void simulationPeriodic() {}
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,22 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.commands.ControllerDriveCommand;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.LimelightUtil;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.SwerveDrive;
+
+import java.time.Clock;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -19,45 +32,128 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    private Clock clock = null;
+    public T34XboxController ctrl = null;
+    public SwerveDrive swerve_drive = null;
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    public Shooter shooter = null;
+    // public Climber climber = null;
+    public TrajMath traj_math = null;
+    public LimelightUtil limelight_util = null;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  }
+    public double arm_angle_setpoint;
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    public double auto_start_dist_1;
+    public double auto_start_dist_2;
+    public double auto_end_dist_1;
+    public double auto_end_dist_2;
+    public double auto_current_dist;
+    public boolean auto_finished_driving_1;
+    public boolean auto_finished_driving_2;
+    public boolean auto_finished_aiming;
+    public boolean auto_finished_shooting;
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
+    public SendableChooser<String> path_chooser = null;
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+    public ControllerDriveCommand DefaultCommand = null;
+
+
+    // // The robot's subsystems and commands are defined here...
+    // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+    // // Replace with CommandPS4Controller or CommandJoystick if needed
+    // private final CommandXboxController m_driverController =
+    //     new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+        this.clock = Clock.systemUTC();
+        this.swerve_drive = new SwerveDrive();
+        this.ctrl = new T34XboxController(0);
+        this.shooter = new Shooter();
+        // this.climber = new Climber();
+        this.traj_math = new TrajMath(
+            23.884,
+            1.9815,
+            1.435,
+            0.2688,
+            ((this.shooter.getTopArmEncoderVal() + this.shooter.getBottomArmEncoderVal()) * 0.5) / Constants.ARM_DEG_SCALAR,
+            30.0
+        );
+        this.limelight_util = new LimelightUtil(
+            this.traj_math,
+            LimelightUtil.TargetMode.kSpeaker
+        );
+        this.arm_angle_setpoint = 90.0;
+        this.auto_start_dist_1 = 0.0;
+        this.auto_end_dist_1 = 0.0;
+        this.auto_start_dist_2 = 0.0;
+        this.auto_end_dist_2 = 0.0;
+        this.auto_finished_driving_1 = false;
+        this.auto_finished_aiming = true;
+        this.auto_finished_driving_2 = true;
+        this.auto_finished_shooting = true;
+        this.DefaultCommand = new ControllerDriveCommand(this.swerve_drive, this.ctrl, this.clock);
+        
+        this.ctrl.setAllAxisDeadband(0.2);
+        // Configure the trigger bindings
+        this.configureBindings();
+
+        this.path_chooser.setDefaultOption("None", "None");
+        this.path_chooser.addOption("Faris room path", "TestingPathFarisRoom");
+    
+        this.path_chooser.addOption("Start at left, score 2 points", "Left_Score1_NoAmp_MoveOut");
+        this.path_chooser.addOption("Start at left, score 3 points", "Left_Score2_NoAmp_MoveOut");
+        this.path_chooser.addOption("Start at left, score 4 points", "Left_Score3_NoAmp_MoveOut");
+    
+        this.path_chooser.addOption("Start at mid, score 2 points", "Mid_Score1_NoAmp_MoveOut");
+        this.path_chooser.addOption("Start at mid, score 3 points", "Mid_Score2_NoAmp_MoveOut");
+        this.path_chooser.addOption("Start at mid, score 4 points", "Mid_Score3_NoAmp_MoveOut");
+    
+        this.path_chooser.addOption("Start at right, score 2 points", "Right_Score1_NoAmp_MoveOut");
+        this.path_chooser.addOption("Start at right, score 3 points", "Right_Score2_NoAmp_MoveOut");
+        this.path_chooser.addOption("Start at right, score 4 points", "Right_Score3_NoAmp_MoveOut");
+    
+        this.path_chooser.addOption("Start at left, score in amp", "Left_Score0_YesAmp_MoveAuto");
+        this.path_chooser.addOption("Start at mid, score in amp", "Middle_Score0_YesAmp_MoveAuto");
+        this.path_chooser.addOption("Start at left, score in amp", "Right_Score0_YesAmp_MoveAuto");
+    }
+
+    /**
+     * Use this method to define your trigger->command mappings. Triggers can be created via the
+     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+     * predicate, or via the named factories in {@link
+     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+     * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+     * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+     * joysticks}.
+     */
+    private void configureBindings() {
+      // // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+      // new Trigger(m_exampleSubsystem::exampleCondition)
+      //     .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+      // // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+      // // cancelling on release.
+      // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // // An example command will be run in autonomous
+        // return Autos.exampleAuto(m_exampleSubsystem);
+
+        return new InstantCommand(() -> {
+          this.swerve_drive.drive(new Translation2d(0.0, -1.0), 0.0);
+        }, this.swerve_drive);
+
+        // final String path_file_name = this.path_chooser.getSelected();
+        // final PathPlannerPath path = PathPlannerPath.fromPathFile(path_file_name);
+    
+        // return AutoBuilder.followPath(path);
+    }
 }
